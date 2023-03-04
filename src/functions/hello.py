05b040ec85253fb6f20b1eb19fce3148fd32d57b
danny n <daniel.noest@disneystreaming.com>
2023-03-04 14:58:13 -0500

    remove hello

--- src/functions/hello.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def hello(event):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": json.dumps(event)
-    }
-
-    return body
--- src/handlers.py
+++ src/handlers.py
@@ -1,17 +1,5 @@
 import json
 
-from src.functions.hello import hello
-
-
-def hello_handler(event, context):
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(hello(event))
-    }
-
-    return response
-
 
 def an_endpoint_handler(event, context):
     
