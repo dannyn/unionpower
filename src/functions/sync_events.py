14b97d582c16932d6714a4f9dd9bb4d0f8707218
danny n <daniel.noest@disneystreaming.com>
2023-03-08 21:29:19 -0500

    Events now sync

--- src/functions/sync_events.py
+++ src/functions/sync_events.py
@@ -6,16 +6,18 @@ from pyairtable import Table
 from src.lib.action_network import Action
 
 def handler(event, context):
-
     key = os.getenv('AIRTABLE_API_KEY')
-    events = Table(key, 'applfZpncSpD2xDJK', 'Events').all()
+    table = Table(key, 'applfZpncSpD2xDJK', 'Events')
+    events = table.all()
     actions = Action.all()
 
+    # check that this is just cause
+    actions = list(filter(lambda x: x.magic_string('justcausecampaign'), actions))
     for action in actions:
         e = action.get_event()
         # remove existing events
-        f = list(filter(lambda x: e['Url'] == x['Url'], events['fields']))
+        f = list(filter(lambda x: e['Url'] == x['fields']['Url'], events))
         if not f:
-            events.create(e)
+            table.create(e)
 
     return { "statusCode": 200, }
