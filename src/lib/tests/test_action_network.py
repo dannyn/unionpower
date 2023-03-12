54d6d47f7cd8e9ceeb4e8b437e686c154a2eb034
danny n <daniel.noest@disneystreaming.com>
2023-03-11 22:08:28 -0500

    phone as int

--- src/lib/tests/test_action_network.py
+++ src/lib/tests/test_action_network.py
@@ -8,15 +8,13 @@ from src.lib.action_network import Signup, Action
 class TestSignup(unittest.TestCase):
     def test_get_action(self):
         signup = Signup.from_file("src/lib/tests/data/event_signup.json")
-        expectedAction = {
-            'worked': True
-        }
+        expectedAction = Action({'worked': True})
 
         with requests_mock.Mocker() as m:
             m.get('https://actionnetwork.org/api/v2/events/87a34885-6ad0-42e0-81af-e38cea2f7548',
-                  json=expectedAction)
+                  json={'worked': True})
             action = signup.get_action()
-            self.assertEqual(action, expectedAction)
+            self.assertEqual(action.data, expectedAction.data)
 
     def test_get_email(self):
         signup = Signup.from_file("src/lib/tests/data/event_signup.json")
@@ -30,7 +28,7 @@ class TestSignup(unittest.TestCase):
             "First Name": 'PersonName',
             "Last Name": 'FamilyName',
             "Phone": '12125551212',
-            "Zip Code": '11218',
+            "Zip Code": 11218,
         }
 
         self.assertEqual(signup.get_volunteer(), expected)
